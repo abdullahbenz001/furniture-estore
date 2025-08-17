ee14f43397d7e4e026b7d02bbb40067bb749f308
Abdullah Benz <Abdullaharnold36@gmail.com>
2025-08-17 23:57:18 +0300

    add sonner and edits

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const isPublicRoute = createRouteMatcher(['/','/about','/products(.*)'])
+const isPublicRoute = createRouteMatcher(['/','/about','/products(.*)','/sign-in(.*)'])
 
 export default clerkMiddleware(async (auth, req) => {
   if (!isPublicRoute(req)) {
